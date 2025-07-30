0d322a805dff739267c5decb84fd5654760c7852
gary909 <25279957+gary909@users.noreply.github.com>
2025-07-30 14:27:11 +0200

    code

--- Moonbase_VR/AsteroidHit.cs
+++ Moonbase_VR/AsteroidHit.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AsteroidHit : MonoBehaviour
 {
     [SerializeField] private GameObject asteroidExplosion;
     [SerializeField] private GameController gameController;
+    [SerializeField] private GameObject popupCanvas;
+
 
     private void Awake()
     {
@@ -16,14 +19,27 @@ public class AsteroidHit : MonoBehaviour
     {
         Instantiate(asteroidExplosion, transform.position, transform.rotation);
 
-        //calculate score for hitting this asteroid.
-        float distanceFromPlayer = Vector3.Distance(transform.position, Vector3.zero);
-        int bonusPoints = (int)distanceFromPlayer;
+        if(GameController.currentGameStatus == GameController.GameState.Playing)
+        {
+            //calculate score for hitting this asteroid.
+            float distanceFromPlayer = Vector3.Distance(transform.position, Vector3.zero);
+            int bonusPoints = (int)distanceFromPlayer;
 
-        int asteroidScore = 10 * bonusPoints;
+            int asteroidScore = 10 * bonusPoints;
 
-        //pass score to GameController
-        gameController.UpdatePlayerScore(asteroidScore);
+            //Set our text for the popup - then instantiate popup
+            popupCanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = asteroidScore.ToString();
+
+            GameObject asteroidPopup = Instantiate(popupCanvas, transform.position, Quaternion.identity);
+
+            //Adjust scale of popup
+            asteroidPopup.transform.localScale = new Vector3(transform.localScale.x * (distanceFromPlayer / 10),
+                                                             transform.localScale.y * (distanceFromPlayer / 10),
+                                                             transform.localScale.z * (distanceFromPlayer / 10));
+
+            //pass score to GameController
+            gameController.UpdatePlayerScore(asteroidScore);
+        }
 
         Destroy(this.gameObject);
     }
--- Moonbase_VR/GameController.cs
+++ Moonbase_VR/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -9,10 +10,27 @@ public class GameController : MonoBehaviour
     [SerializeField] private float gameTime;
     private float sliderCurrentFillAmount = 1f;
 
+    [Header("Score Components")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+
     private int playerScore;
+
+    public enum GameState
+    {
+        Waiting,
+        Playing,
+        GameOver
+    }
+    public static GameState currentGameStatus;
+
+    private void Awake()
+    {
+        currentGameStatus = GameState.Waiting;
+    }
 
     private void Update()
     {
+        if(currentGameStatus == GameState.Playing)
         AdjustTimer();
     }
 
@@ -25,6 +43,10 @@ public class GameController : MonoBehaviour
 
     public void UpdatePlayerScore(int asteroidHitPoints)
     {
-        playerScore += asteroidHitPoints;
+        if (currentGameStatus != GameState.Playing)
+            return;
+
+        playerScore += asteroidHitPoints; 
+        scoreText.text = playerScore.ToString();
     }
 }
